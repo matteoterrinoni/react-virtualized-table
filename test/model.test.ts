4fafbd7107aa1c738f832eee3f0a5c56d8e5b326
Matteo Terrinoni <matteo.terrinoni@gmail.com>
2018-05-12 10:26:19 +0200

    test($test):

---
+++ test/model.test.ts
@@ -0,0 +1,11 @@
+import { initCounter } from '../src/model'
+
+describe('Method over lists', () => {
+  it('When init the counter each element as a value of zero ', () => {
+    const counter = initCounter()
+    const lengthOfElementsHigherThanZero = Object.keys(counter).filter(
+      k => counter[k] > 0
+    ).length
+    expect(lengthOfElementsHigherThanZero == 0).toBeTruthy()
+  })
+})
