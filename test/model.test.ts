03bbbc92fc09454edafab0c3b6958905c5cdacce
Matteo Terrinoni <matteo.terrinoni@gmail.com>
2018-05-14 18:01:35 +0200

    test(model): testo model main methods

--- test/model.test.ts
+++ test/model.test.ts
@@ -1,11 +1,106 @@
-import { initCounter } from '../src/model'
+import { initCounter, Item, Given, Sort, sortTypes, Column } from '../src/model'
+
+import { FakeItem, initFakeItems, fakeItem, fakeItemColumns } from './utils'
+
+const testname = 'mytestName'
 
 describe('Method over lists', () => {
   it('When init the counter each element as a value of zero ', () => {
     const counter = initCounter()
-    const lengthOfElementsHigherThanZero = Object.keys(counter).filter(
-      k => counter[k] > 0
-    ).length
+    const lengthOfElementsHigherThanZero = Object.keys(counter).filter(k => counter[k] > 0).length
     expect(lengthOfElementsHigherThanZero === 0).toBeTruthy()
+  })
+
+  it('When get a list with one item checked it returns a Counter object with the right count', () => {
+    let counter = initCounter()
+
+    let items = initFakeItems(10) as Item<FakeItem>[]
+
+    items[0].checked = true
+
+    Given.items(items).filter('', null, counter)
+
+    expect(counter.checked === 1).toBeTruthy()
+  })
+
+  it('filter correctly', () => {
+    let counter = initCounter()
+
+    let items = initFakeItems(10) as Item<FakeItem>[]
+    items.push(fakeItem(items[0]).withName(testname).result)
+
+    Given.items(items).filter(testname, null, counter)
+    expect(counter.visible === 0).toBeTruthy()
+
+    Given.items(items).filter(testname.toLowerCase(), null, counter)
+    expect(counter.visible === 1).toBeTruthy()
+  })
+
+  it('sort correctly', () => {
+    let counter = initCounter()
+
+    let items = initFakeItems(2) as Item<FakeItem>[]
+    items[0] = fakeItem(items[0]).withName('A').result
+    items[1] = fakeItem(items[1]).withName('B').result
+
+    const sortedDesc = Given.items(items).sort({ name: 'desc' }, fakeItemColumns).result
+    expect(sortedDesc[0].name === 'B').toBeTruthy()
+
+    const sortedAsc = Given.items(items).sort({ name: 'asc' }, fakeItemColumns).result
+    expect(sortedAsc[0].name === 'A').toBeTruthy()
+  })
+
+  it('sorting the column will change correctly the Sort model', () => {
+    let sorting: Sort = {}
+
+    Given.column(fakeItemColumns[0]).sort(sorting)
+    expect(sorting[fakeItemColumns[0].name] === sortTypes.asc.key).toBeTruthy()
+
+    Given.column(fakeItemColumns[0]).sort(sorting)
+    expect(sorting[fakeItemColumns[0].name] === sortTypes.desc.key).toBeTruthy()
+
+    Given.column(fakeItemColumns[0]).sort(sorting)
+    expect(sorting[fakeItemColumns[0].name] === undefined).toBeTruthy()
+  })
+
+  it('default sorting will setup correctly the Sort model', () => {
+    let columns: Column<FakeItem>[] = [
+      {
+        name: 'name',
+        render: v => v,
+        fn: (a, b) => null
+      },
+      {
+        name: 'age',
+        render: v => v
+      },
+      {
+        name: 'email',
+        render: v => v,
+        fn: (a, b) => null
+      }
+    ]
+
+    const sortingByName: Sort = Given.columns(columns).getDefaultSorting()
+    expect(sortingByName['name'] === sortTypes.asc.key).toBeTruthy()
+
+    delete columns[0].fn
+    const sortingByEmail: Sort = Given.columns(columns).getDefaultSorting()
+    expect(sortingByEmail['email'] === sortTypes.asc.key).toBeTruthy()
+  })
+
+  it('clean is cleaning items from other info', () => {
+    let counter = initCounter()
+    let items = initFakeItems(10) as Item<FakeItem>[]
+    items[0] = fakeItem(items[0]).withName(testname).result
+    items[0].checked = true
+    Given.items(items).filter(testname.toLowerCase(), null, counter)
+
+    expect(items[0].checked === true).toBeTruthy()
+    expect(items[0].visible === true).toBeTruthy()
+
+    const cleanedItems = Given.items(items).clean().result
+    expect(items[0].checked !== true).toBeTruthy()
+    expect(items[0].visible !== true).toBeTruthy()
   })
 })
---
+++ test/utils.ts
@@ -0,0 +1,63 @@
+import * as faker from 'faker'
+
+import { VTableColumn } from '../src/react-virtualized-table'
+
+export const textComparison = (a, b, prop: (a: any) => any) => {
+  const _a = JSON.stringify(prop(a))
+    .toLowerCase()
+    .trim()
+  const _b = JSON.stringify(prop(b))
+    .toLowerCase()
+    .trim()
+  return _a < _b ? -1 : _a > _b ? 1 : 0
+}
+
+export type FakeItem = {
+  name: string
+  age: number
+  email: string
+}
+
+export const initFakeItems = (n: number = 1000) => {
+  let list: FakeItem[] = []
+
+  for (let i = 0; i < n; i++) {
+    list.push({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      age: faker.random.number()
+    })
+  }
+
+  return list
+}
+
+export const fakeItem = (_f?: FakeItem) => {
+  let f = _f || ({} as FakeItem)
+  return {
+    withName: (name: string) => fakeItem({ ...f, name }),
+
+    withAge: (age: number) => fakeItem({ ...f, age }),
+
+    withEmail: (email: string) => fakeItem({ ...f, email }),
+
+    result: f
+  }
+}
+
+export const fakeItemColumns: VTableColumn<FakeItem>[] = [
+  {
+    name: 'name',
+    render: v => v,
+    fn: (a, b) => textComparison(a, b, o => o)
+  },
+  {
+    name: 'age',
+    render: v => v
+  },
+  {
+    name: 'email',
+    render: v => v,
+    fn: (a, b) => textComparison(a, b, o => o)
+  }
+]
