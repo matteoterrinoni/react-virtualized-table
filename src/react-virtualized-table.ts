ff0d764f32a9aef6b2e1bbedfea4b38b62f19efe
Matteo Terrinoni <matteo.terrinoni@gmail.com>
2019-01-31 11:51:33 +0100

    fix(stickyWrapper): fix typo stycky to sticky

--- src/react-virtualized-table.ts
+++ src/react-virtualized-table.ts
@@ -2,4 +2,4 @@ export { default as FilteredVTable } from './filtered/index'
 
 export { VTable, VTableItem, VTableRenderItemProps, GivenVTable, VTableColumn } from './index'
 
-export { default as Stycky } from './stickyWrapper'
+export { default as Sticky } from './stickyWrapper'
