936e1eabfed6d40627dedc317fee9e64676367dd
Matteo Terrinoni <matteo.terrinoni@gmail.com>
2019-01-31 11:30:30 +0100

    feat(stickyWrapper): export sticky component
    export sticky wrapper component

--- src/react-virtualized-table.ts
+++ src/react-virtualized-table.ts
@@ -1,3 +1,5 @@
 export { default as FilteredVTable } from './filtered/index'
 
 export { VTable, VTableItem, VTableRenderItemProps, GivenVTable, VTableColumn } from './index'
+
+export { default as Stycky } from './stickyWrapper'
